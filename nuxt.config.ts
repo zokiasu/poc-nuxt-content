1d119f84ccd7850506a09bce3b9a10383b72bf4c
Pierrick Taily <pierrick.tly@gmail.com>
2025-03-12 18:07:18 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/content'],
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
 })
