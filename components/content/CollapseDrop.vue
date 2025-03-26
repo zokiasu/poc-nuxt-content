1b6056ebad8ae9f0562dc73463fac71e091ea4d3
Pierrick <pierrick.tly@gmail.com>
2025-03-26 10:46:57 +0100

    test

---
+++ components/content/CollapseDrop.vue
@@ -0,0 +1,19 @@
+<template>
+  <UCollapsible class="flex flex-col gap-2 w-48">
+    <UButton
+      label="Open"
+      color="neutral"
+      variant="subtle"
+      trailing-icon="i-lucide-chevron-down"
+      block
+    />
+
+    <template #content>
+      <ContentSlot name="content" />
+    </template>
+  </UCollapsible>
+</template>
+
+<script setup lang="ts">
+
+</script>
